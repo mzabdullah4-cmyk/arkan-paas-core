7b555923957f98c132e34865abe9aedc7a586669
mzabdullah4-cmyk <mzabdullah4@gmail.com>
2026-09-18 11:31:43 +0200

    Delete control-plane/ontrol-plane/app/main.py

--- control-plane/ontrol-plane/app/main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI(title="Arkan Control Plane", version="1.0.0")
-
-@app.get("/")
-def read_root():
-    return {"status": "online", "service": "Arkan API Gateway", "security": "air-gapped"}
